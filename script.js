f7be978d57ff5a2a33f0c317a036071a183b297e
Yasin Rabiee <hlww6303@gmail.com>
2024-09-21 08:35:16 -0700

    add localStorage

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 function changeTime() {
-  let date = new Date();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
-  let second = date.getSeconds();
+	let date = new Date();
+	let hour = date.getHours();
+	let minute = date.getMinutes();
+	let second = date.getSeconds();
 
-  if (hour > 12) hour -= 12;
+	if (hour > 12) hour -= 12;
 
-  let hourDegree = hour * 30 + minute / 2;
-  $(`.hour`).css(`transform`, ` rotate(${hourDegree}deg)`);
+	let hourDegree = hour * 30 + minute / 2;
+	$(`.hour`).css(`transform`, ` rotate(${hourDegree}deg)`);
 
-  let minuteDegree = minute * 6;
-  $(`.minute`).css(`transform`, ` rotate(${minuteDegree}deg)`);
+	let minuteDegree = minute * 6;
+	$(`.minute`).css(`transform`, ` rotate(${minuteDegree}deg)`);
 
-  let secondDegree = second * 6;
-  $(`.second`).css(`transform`, ` rotate(${secondDegree}deg)`);
+	let secondDegree = second * 6;
+	$(`.second`).css(`transform`, ` rotate(${secondDegree}deg)`);
 }
 
 changeTime();
 
 setInterval(function () {
-  changeTime();
+	changeTime();
 }, 1000);
 
+if(localStorage.getItem('mode') == `dark`)
+	$(`body`).addClass(`dark`)
+else
+	$(`body`).addClass(`light`)
+
 $(`.mode-switch`).click(function () {
-  $(`body`).toggleClass(`dark`);
-  if ($(`body`).hasClass(`dark`)) $(this).html(`Light Mode`);
-  else $(this).html(`Dark Mode`);
+	$(`body`).toggleClass(`dark`);
+	if ($(`body`).hasClass(`dark`))
+	{
+		$(this).html(`Light Mode`);
+		localStorage.setItem(`mode`,`dark`)
+	}
+	else
+	{
+		$(this).html(`Dark Mode`);
+		localStorage.setItem(`mode`,`light`)
+	}	
 });
